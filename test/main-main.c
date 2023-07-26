153e8bde4427e37d644b307d239b20b0d265f151
faithakinbobola <faithakinbobola2@gmail.com>
2023-07-25 17:04:02 -0700

    printf

---
+++ test/main-main.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("Unknown:[%r]\n", "hello");
+    return (0);
+}
